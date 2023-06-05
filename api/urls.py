5c323e8f5835e9bf8f62f51ef199ccd812b54003
Mark O'Connor <mark.oconnor14@gmail.com>
2023-06-05 19:12:16 +0800

    feat: finish crud for items, posts, and students

--- api/urls.py
+++ api/urls.py
@@ -2,11 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getData),
-    path('add/', views.addItem),
-    path('posts/', views.getPosts),
+
+    path('get-items', views.getItems),
+    path('add-item/', views.addItem),
+    path('delete-item/<str:pk>/', views.deleteItem),
+
+    path('get-posts/', views.getPosts),
     path('add-post/', views.addPost),
-    path('get-students/', views.getStudents),
-    path('add-student/', views.addStudent),
-    path('delete-student/<str:pk>/', views.deleteStudent, name="student-delete"),
+    path('delete-post/<str:pk>/', views.deletePost, name="post-delete"),
+
+    path('get-students/', views.getStudents, name="get-students"),
+    path('get-student/<str:pk>/', views.getStudentById, name="get-student"),
+    path('add-student/', views.addStudent, name="add-student"),
+    path('update-student/<str:pk>/', views.updateStudent, name="update-student"),
+    path('delete-student/<str:pk>/', views.deleteStudent, name="delete-student"),
 ]
--- api/views.py
+++ api/views.py
@@ -7,9 +7,10 @@ from .serializers import ItemSerializer, PostSerializer, StudentSerializer
 
 # ITEMS VIEWS (FROM TUTORIAL)
 @api_view(['GET'])
-def getData(request):
+def getItems(request):
     items = Item.objects.all()
     serializer = ItemSerializer(items, many=True)
+
     return Response(serializer.data)
 
 @api_view(['POST'])
@@ -17,7 +18,15 @@ def addItem(request):
     serializer = ItemSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
+
     return Response(serializer.data)
+
+@api_view(['DELETE'])
+def deleteItem(request, pk):
+    item = Item.objects.get(id=pk)
+    item.delete()
+
+    return Response("Item deleted successfully!")
 
 
 # POST VIEWS
@@ -25,6 +34,7 @@ def addItem(request):
 def getPosts(request):
     posts = Post.objects.all()
     serializer = PostSerializer(posts, many=True)
+
     return Response(serializer.data)
 
 @api_view(['POST'])
@@ -32,27 +42,58 @@ def addPost(request):
     serializer = PostSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
+
     return Response(serializer.data)
 
+@api_view(['DELETE'])
+def deletePost(request, pk):
+    post = Post.objects.get(id=pk)
+    post.delete()
+
+    return Response('Post deleted successfully!')
+
 # STUDENT VIEWS
+# GET ALL STUDENTS
 @api_view(['GET'])
 def getStudents(request):
     students = Student.objects.all()
     serializer = StudentSerializer(students, many=True)
+
     return Response(serializer.data)
 
+# GET STUDENT BY ID
+@api_view(['GET'])
+def getStudentById(request, pk):
+    student = Student.objects.get(id=pk)
+    serializer = StudentSerializer(student, many=False)
+
+    return Response(serializer.data)
+
+
+# CREATE NEW STUDENT
 @api_view(['POST'])
 def addStudent(request):
     serializer = StudentSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
+
     return Response(serializer.data)
 
+# UPDATE STUDENT
+@api_view(['POST'])
+def updateStudent(request, pk):
+    student = Student.objects.get(id=pk)
+    serializer = StudentSerializer(instance=student, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+# DELETE STUDENT
 @api_view(['DELETE'])
 def deleteStudent(request, pk):
     student = Student.objects.get(id=pk)
     student.delete()
-    return Response('Item successfully delete!')
-    # serializer - StudentSerializesr(data=request.data)
-    # if
+
+    return Response('Item successfully deleted!')
 
