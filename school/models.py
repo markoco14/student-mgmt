20db057e108cae10558fb700db8ce96ccdd6c5be
Mark O'Connor <mark.oconnor14@gmail.com>
2023-06-20 15:56:17 +0800

    fix: fix owner field again

--- school/models.py
+++ school/models.py
@@ -2,8 +2,10 @@ from django.db import models
 from user.models import User
 
 # Create your models here.
+
+
 class School(models.Model):
     name = models.CharField(max_length=50)
-    owner_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
