46207ceda48b7de67a9e757e14abdea42f592c62
Mark O'Connor <mark.oconnor14@gmail.com>
2023-06-20 15:54:54 +0800

    fix: change owner field to owner_id field to fix name error in School model

--- school/models.py
+++ school/models.py
@@ -4,6 +4,6 @@ from user.models import User
 # Create your models here.
 class School(models.Model):
     name = models.CharField(max_length=50)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner_id = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
