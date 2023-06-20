81b0a8b23eda81a23d36049729e0ac5c19186afd
Mark O'Connor <mark.oconnor14@gmail.com>
2023-06-20 10:39:19 +0800

    feature: add timestamps to school and user models

--- school/models.py
+++ school/models.py
@@ -5,3 +5,5 @@ from user.models import User
 class School(models.Model):
     name = models.CharField(max_length=50)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
--- user/models.py
+++ user/models.py
@@ -6,3 +6,5 @@ from django.db import models
 class User(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
