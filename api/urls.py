d67a2e550459f7e6592988c3bd29e8729796d3fc
Mark O'Connor <mark.oconnor14@gmail.com>
2023-06-25 18:50:41 +0800

    chore: change named route for get schools by owner id

--- api/urls.py
+++ api/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('add-user/', views.addUser, name="add-user"),
 
     # SCHOOL ROUTES
-    path('get-schools/<str:pk>/', views.getSchools, name="get-schools"),
+    path('get-schools/<str:pk>/', views.getSchools, name="get-schools-by-owner"),
     path('add-school/', views.addSchool, name="add-school"),
     path('delete-school/<str:pk>/', views.deleteSchool, name="delete-school"),
     path('update-school/<str:pk>/', views.updateSchool, name="update-school"),
