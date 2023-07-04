7d4119ae78a96788a3e0fd036455fed54b4b1776
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-04 17:25:39 +0800

    feature: create custom user model per django recommendations

--- user/models.py
+++ user/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
+from .managers import CustomUserManager
 
-class User(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+
