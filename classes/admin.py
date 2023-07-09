72e60056cfa01120043b8b2720ea866064f7ab35
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-09 10:38:26 +0800

    feature: copy classes module from broken project

---
+++ classes/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Class, ClassStudent
+
+# Register your models here.
+admin.site.register(Class)
+admin.site.register(ClassStudent)
---
+++ classes/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+from schools.models import School
+from students.models import Student
+
+# Create your models here.
+
+
+class Class(models.Model):
+    name = models.CharField(max_length=50)
+    school_id = models.ForeignKey(
+        School, db_column='school_id', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.TimeField(auto_now=True)
+
+
+class ClassStudent(models.Model):
+    class_id = models.ForeignKey(
+        Class, db_column='class_id', on_delete=models.CASCADE)
+    student_id = models.ForeignKey(
+        Student, db_column='student_id', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.TimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'classes_class_students'
