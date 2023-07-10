b8084275c78f1b582238a728993d5c7cb43032da
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-10 21:06:44 +0800

    feature: remove content field from Report model

--- reports/models.py
+++ reports/models.py
@@ -5,7 +5,6 @@ from classes.models import Class
 # Create your models here.
 class Report(models.Model):
     date = models.DateField(auto_now_add=True)
-    content = models.TextField(max_length=150, default="")
     is_complete = models.BooleanField(default=False)
     class_id = models.ForeignKey(Class, on_delete=models.CASCADE, db_column='class_id', )
     created_at = models.DateTimeField(auto_now_add=True)
