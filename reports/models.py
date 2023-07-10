36e0fd55a3f351ac79622e4225f755b668882ece
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-10 20:51:26 +0800

    feature: add unique constraint for class_id and date fields in Report model

--- reports/models.py
+++ reports/models.py
@@ -11,4 +11,5 @@ class Report(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-   
+    class Meta:
+        unique_together=['class_id', 'date']
