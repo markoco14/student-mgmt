83ce95a6377f91213f1d7dfe97dea12775dc0a51
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-11 08:46:31 +0800

    feature: run ReportDetails migration

--- reports/admin.py
+++ reports/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from reports.models import Report
+from reports.models import Report, ReportDetails
 
 # Register your models here.
 admin.site.register(Report)
+admin.site.register(ReportDetails)
