2b80c98542648d92d77e10e823525bada43e5884
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-14 21:18:36 +0800

    feature: add core app to project

--- school_mgmt/settings.py
+++ school_mgmt/settings.py
@@ -137,29 +137,29 @@ WSGI_APPLICATION = 'school_mgmt.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 # LOCAL DB DEVELOPMENT CREDENTIALS
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': os.environ.get('DEV_DB_NAME'),
-#         'USER': os.environ.get('DEV_DB_USER'),
-#         'PASSWORD': os.environ.get('DEV_DB_PASSWORD'),
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#     }
-# }
-
-# PSCALE DEVELOPMENT DB CREDENTIALS
 DATABASES = {
     'default': {
-        'ENGINE': 'django_psdb_engine',
-        'NAME': os.environ.get('DB_NAME'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'OPTIONS': {'ssl': {'ca': os.environ.get('MYSQL_ATTR_SSL_CA')}}
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DEV_DB_NAME'),
+        'USER': os.environ.get('DEV_DB_USER'),
+        'PASSWORD': os.environ.get('DEV_DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
+# PSCALE DEVELOPMENT DB CREDENTIALS
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django_psdb_engine',
+#         'NAME': os.environ.get('DB_NAME'),
+#         'HOST': os.environ.get('DB_HOST'),
+#         'PORT': os.environ.get('DB_PORT'),
+#         'USER': os.environ.get('DB_USER'),
+#         'PASSWORD': os.environ.get('DB_PASSWORD'),
+#         'OPTIONS': {'ssl': {'ca': os.environ.get('MYSQL_ATTR_SSL_CA')}}
+#     }
+# }
 
 
 # Password validation
