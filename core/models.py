4ffa5001f83e2f3e93fdafa86310bf08a0791a52
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-14 21:19:02 +0800

    feature: add Weekday model to core app

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Weekday(models.Model):
+    DAYS_OF_WEEK = [
+        (0, 'Monday'),
+        (1, 'Tuesday'),
+        (2, 'Wednesday'),
+        (3, 'Thursday'),
+        (4, 'Friday'),
+        (5, 'Saturday'),
+        (6, 'Sunday'),
+    ]
+
+    day = models.IntegerField(choices=DAYS_OF_WEEK)
+
+    def __str__(self):
+        return self.get_day_display()
