9bfc78d09f778b9fb6535ded31bd96645923112c
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-14 20:58:33 +0800

    feature: add level model to Levels app

--- levels/models.py
+++ levels/models.py
@@ -3,3 +3,7 @@ from django.db import models
 from schools.models import School
 
 # Create your models here.
+
+class Level(models.Model):
+    name = models.CharField(max_length=200, required=True)
+    school = models.ForeignKey(School, related_name='levels', on_delete=models.CASCADE)
