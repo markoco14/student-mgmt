2f0e144394a0c4b7d734745f6087a1f3dd9a0aff
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-16 12:07:55 +0800

    feature: make change to weekday model

--- core/models.py
+++ core/models.py
@@ -4,13 +4,13 @@ from django.db import models
 
 class Weekday(models.Model):
     DAYS_OF_WEEK = [
-        (0, 'Monday'),
-        (1, 'Tuesday'),
-        (2, 'Wednesday'),
-        (3, 'Thursday'),
-        (4, 'Friday'),
-        (5, 'Saturday'),
-        (6, 'Sunday'),
+        (1, 'Monday'),
+        (2, 'Tuesday'),
+        (3, 'Wednesday'),
+        (4, 'Thursday'),
+        (5, 'Friday'),
+        (6, 'Saturday'),
+        (7, 'Sunday'),
     ]
 
     day = models.IntegerField(choices=DAYS_OF_WEEK)
