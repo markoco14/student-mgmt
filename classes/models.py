e3bd5a15d6ac68a36d0b24b850cba7cfd7b3d1cd
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-27 15:35:18 +0800

    feature: update Class model allow level field = null

--- classes/models.py
+++ classes/models.py
@@ -12,7 +12,7 @@ class Class(models.Model):
     name = models.CharField(max_length=200)
     school_id = models.ForeignKey(
         School, db_column='school_id', on_delete=models.CASCADE)
-    level = models.ForeignKey(Level, db_column="level", related_name="classes", on_delete=models.SET_DEFAULT, default="")
+    level = models.ForeignKey(Level, db_column="level", related_name="classes", on_delete=models.SET_DEFAULT, default="", null=True)
     day = models.ManyToManyField(Weekday, related_name="classes")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
