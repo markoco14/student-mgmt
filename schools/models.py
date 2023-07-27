505ef0c6831d0e6120636b438fcb281a12e31721
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-27 19:05:10 +0800

    feature: update SchoolUser model with on_delete=CASCADE

--- schools/models.py
+++ schools/models.py
@@ -12,12 +12,12 @@ class School(models.Model):
 
 
 class SchoolUser(models.Model):
-    school = models.ForeignKey(School, db_column='school', on_delete=models.DO_NOTHING)
-    user = models.ForeignKey(User, db_column='user', on_delete=models.DO_NOTHING)
+    school = models.ForeignKey(School, db_column='school', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, db_column='user', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'schools_school_users'
-        unique_together = ['school_id', 'user_id']
+        unique_together = ['school', 'user']
         verbose_name_plural = 'School users'
