b530b8ad0e7283dcf5aded43fc11998f7c9124b4
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-27 18:10:13 +0800

    feature: register SchoolUsers model in admin

--- schools/admin.py
+++ schools/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import School
+from .models import School, SchoolUsers
 
 # Register your models here.
 admin.site.register(School)
+admin.site.register(SchoolUsers)
