5abf6feb7dd5b8001fba80a77d7cbff6c6ec4dac
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-17 20:50:30 +0800

    refactor: move User related views to views/user_views.py

---
+++ api/views/user_views.py
@@ -0,0 +1,83 @@
+from api.serializers import SchoolUserSerializer, TeacherSerializer, UserSerializer
+from schools.models import SchoolUser
+from users.models import Teacher, User
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+@api_view(['GET'])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+
+    return Response(serializer.data)
+
+# ADD NEW USER
+
+
+@api_view(['POST'])
+def addUser(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+# ADD NEW TEACHER
+
+@api_view(['POST'])
+def addTeacher(request):
+    try:
+        # check if the user exists at all
+        user = User.objects.get(email=request.data['email'])
+        school_user = {
+            "school": request.data['school'],
+            "user": user.id
+        }
+        serializer = SchoolUserSerializer(data=school_user)
+        if serializer.is_valid():
+            serializer.save()
+        
+        user_data = {
+            "id": user.id,
+            "email": user.email,
+            "first_name": user.first_name,
+            "last_name": user.last_name
+        }
+
+        teacher_serializer = TeacherSerializer(data=user_data, many=False)
+        
+        if teacher_serializer.is_valid():
+            print('ok')
+
+        return Response(teacher_serializer.data)
+
+    except User.DoesNotExist:
+        serializer = TeacherSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            school_user = {
+                "school": request.data['school'],
+                "user": serializer.data['id']
+            }
+            school_user_serializer = SchoolUserSerializer(data=school_user)
+            if school_user_serializer.is_valid():
+                school_user_serializer.save()
+
+            return Response(serializer.data)
+
+        
+# GET TEACHERS FOR SPECIFIC SCHOOL
+
+@api_view(['GET'])
+def getTeachersBySchool(request, school_pk, owner_pk):
+    school_users = SchoolUser.objects.filter(school=school_pk).exclude(user=owner_pk)
+    
+    user_ids = []
+    for school_user in school_users:
+        user_ids.append(school_user.user)
+
+    users = Teacher.objects.filter(email__in=user_ids)
+    serializer = TeacherSerializer(users, many=True)
+
+    return Response(serializer.data)
