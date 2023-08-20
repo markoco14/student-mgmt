93fcb149ac71871ee19c1a78a746a33d75a85fb6
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-20 14:55:31 +0800

    feature: make Subject model and run migrations

--- curriculum/models.py
+++ curriculum/models.py
@@ -5,5 +5,10 @@ from schools.models import School
 # Create your models here.
 
 class Level(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255) # e.g., "Octopus", "1", "2", ...
     school = models.ForeignKey(School, related_name='levels', on_delete=models.CASCADE)
+
+class Subject(models.Model):
+    name = models.CharField(max_length=255)  # e.g., "Phonics", "Spelling", ...
+    school = models.ForeignKey(School, related_name='subjects', on_delete=models.CASCADE)
+
