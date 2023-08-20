be86551a02b9a991a107a45eb5d26bc6e81f9a0d
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-21 00:38:26 +0800

    feature: register Subject and SubjectLevel in Django admin

--- curriculum/admin.py
+++ curriculum/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from curriculum.models import Level
+from curriculum.models import Level, Subject, SubjectLevel
 
 # Register your models here.
 admin.site.register(Level)
+admin.site.register(Subject)
+admin.site.register(SubjectLevel)
