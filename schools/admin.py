e32e0d8dff775763a4f52ceb872c7d9b3e73ede2
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-21 18:05:03 +0800

    feature: register SchoolDay model in django admin

--- schools/admin.py
+++ schools/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import School, SchoolUser
+from .models import School, SchoolUser, SchoolDay
 
 # Register your models here.
 admin.site.register(School)
 admin.site.register(SchoolUser)
+admin.site.register(SchoolDay)
