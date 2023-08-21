d99a205a2a17baec95aa493baa341aab03665906
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-21 10:21:35 +0800

    feature: add __str__ methods to School and SchoolUser models

--- schools/models.py
+++ schools/models.py
@@ -10,6 +10,9 @@ class School(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"{self.name} (id: {self.id}) Owner: {self.owner_id.first_name} {self.owner_id.last_name} (id: {self.owner_id.id})"
+
 
 class SchoolUser(models.Model):
     school = models.ForeignKey(School, db_column='school', related_name='school_users', on_delete=models.CASCADE)
@@ -21,3 +24,6 @@ class SchoolUser(models.Model):
         db_table = 'schools_school_users'
         unique_together = ['school', 'user']
         verbose_name_plural = 'School users'
+    
+    def __str__(self):
+        return f"{self.user.first_name} (id: {self.user.id}) can access {self.school.name} (id: {self.school.id})"
