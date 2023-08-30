2a72a72932b50dac742a3ab4524dc0933c4649a7
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-30 12:28:14 +0800

    feature: add special ModuleAssessmentPageSerializer

--- curriculum/serializers/assessment_serializers.py
+++ curriculum/serializers/assessment_serializers.py
@@ -1,6 +1,24 @@
 from rest_framework import serializers
-from curriculum.models import Assessment, AssessmentType
+from curriculum.models import Assessment, AssessmentType, Module
+from curriculum.serializers.curriculum_serializers import ModuleSerializer, SubjectLevelListSerializer, SubjectLevelSerializer
 
+
+
+
+
+class ModuleAssessmentPageSerializer(serializers.ModelSerializer):
+    subject_level = SubjectLevelListSerializer()
+    assessments = serializers.SerializerMethodField()
+
+    def get_assessments(self, obj):
+        assessments = Assessment.objects.filter(module=obj.id)
+        serializer = AssessmentSerializer(assessments, many=True)
+
+        return serializer.data
+    
+    class Meta:
+        model = Module
+        fields = '__all__'
 
 
 class AssessmentTypeSerializer(serializers.ModelSerializer):
