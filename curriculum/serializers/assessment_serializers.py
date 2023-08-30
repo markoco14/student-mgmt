0485ec91b0cf0f63b2884b2da5c21f7913df0adc
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-30 09:45:33 +0800

    feature: create AssessmentSerializer

--- curriculum/serializers/assessment_serializers.py
+++ curriculum/serializers/assessment_serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
-from curriculum.models import AssessmentType
+from curriculum.models import Assessment, AssessmentType
 
 
 
 class AssessmentTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = AssessmentType
+        fields = '__all__'
+
+class AssessmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Assessment
         fields = '__all__'
