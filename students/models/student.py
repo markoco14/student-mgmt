f1663f4ffc6fbecbf95708f624800333682f760d
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-02 15:53:01 +0800

    refactor: move Student model to 'student.py' in 'students/models/' folder

---
+++ students/models/student.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from schools.models import School
+from users.models import User
+
+# Create your models here.
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    age = models.SmallIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    school_id = models.ForeignKey(School, on_delete=models.CASCADE, db_column='school_id')
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} (id: {self.id}) in {self.school_id.name} (id: {self.school_id.id})"
