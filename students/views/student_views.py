4773ab80f8c5f86a6f691b4ffbe2f636ebff52b1
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-02 18:18:03 +0800

    feature: add optional pagination to StudentList get request

--- students/views/student_views.py
+++ students/views/student_views.py
@@ -4,48 +4,83 @@ from rest_framework.views import APIView
 from api.serializers.serializers import StudentSerializer
 from rest_framework.exceptions import NotFound
 from students.models.student import Student
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
+
+
 class StudentList(APIView):
     """
     List all Units, or create a new one.
-    """ 
+    """
 
     def get(self, request, school_pk=None, format=None):
-        modules = Student.objects.all()
+        students = Student.objects.all()
 
         # Fetch query parameters
         first_name = request.query_params.get('first_name', None)
         last_name = request.query_params.get('last_name', None)
         age = request.query_params.get('age', None)
         gender = request.query_params.get('gender', None)
+        page = request.query_params.get('page', None)
+        per_page = request.query_params.get('per_page', 20)
 
         # Filter by school (hierachical url)
         if school_pk:
-            modules = modules.filter(school=school_pk)
+            students = students.filter(school=school_pk)
 
         # Further filter by query params
         if first_name:
-            modules = modules.filter(first_name=first_name)
+            students = students.filter(first_name=first_name)
         if last_name:
-            modules = modules.filter(last_name=last_name)
+            students = students.filter(last_name=last_name)
         if age:
-            modules = modules.filter(age=age)
+            students = students.filter(age=age)
         if gender:
-            modules = modules.filter(gender=gender)
+            students = students.filter(gender=gender)
 
-      
+        # check if page number is letters and send response that can be alerted
+        # even though the front end should control for this.
+        
 
-        serializer = StudentSerializer(modules, many=True)
+        if page is not None:
+            
+            try:
+                page = int(page)
+            except ValueError:
+                return Response({"detail": "Page number needs to be an integer greater than 0"})
+            
+
+            paginator = Paginator(students, per_page)
+
+            try:
+                students = paginator.page(page)
+            except PageNotAnInteger:
+                students = paginator.page(1)
+            except EmptyPage:
+                students = paginator.page(paginator.num_pages)
+
+            serializer = StudentSerializer(students, many=True)
+
+            return Response({
+                'count': paginator.count,
+                'total_pages': paginator.num_pages,
+                'current_page': int(page),
+                'per_page': int(per_page),
+                'results': serializer.data
+            })
+
+        serializer = StudentSerializer(students, many=True)
         return Response(serializer.data)
-    
+
     def post(self, request, format=None):
         serializer = StudentSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
+
 class StudentDetail(APIView):
 
     # Utility method to get an object or return a 404 response
@@ -73,7 +108,8 @@ class StudentDetail(APIView):
     # Partially update a specific entry by primary key
     def patch(self, request, student_pk):
         student = self.get_object(student_pk)
-        serializer = StudentSerializer(student, data=request.data, partial=True)
+        serializer = StudentSerializer(
+            student, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
