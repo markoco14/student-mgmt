e36d23d6f1e228f942fa345833eb5c880dc4a13c
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-03 09:11:49 +0800

    feature: add ClassDay model, adjust Class model, and run migrations

--- classes/admin.py
+++ classes/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Class, ClassStudent
+from .models import Class, ClassStudent, ClassDay
 
 # Register your models here.
 admin.site.register(Class)
+admin.site.register(ClassDay)
 admin.site.register(ClassStudent)
--- classes/models.py
+++ classes/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from schedule.models import Weekday
 from curriculum.models import Level
 
-from schools.models import School
+from schools.models import School, SchoolDay
 from students.models import Student
 from users.models import User
 
@@ -12,8 +12,8 @@ from users.models import User
 class Class(models.Model):
     name = models.CharField(max_length=200)
     school = models.ForeignKey(School, on_delete=models.CASCADE)
-    level = models.ForeignKey(Level, related_name="classes", on_delete=models.SET_NULL, null=True, blank=True)
-    day = models.ManyToManyField(Weekday, related_name="classes")
+    level = models.ForeignKey(Level, related_name="classes", on_delete=models.PROTECT)
+    days = models.ManyToManyField(SchoolDay, through='ClassDay', related_name="classes")
     teacher = models.ForeignKey(User, related_name="classes", on_delete=models.SET_NULL, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -24,6 +24,15 @@ class Class(models.Model):
     class Meta:
         verbose_name_plural = 'Classes'
 
+class ClassDay(models.Model):
+    class_id = models.ForeignKey(Class, db_column='class_id', on_delete=models.CASCADE)
+    school_day_id = models.ForeignKey(SchoolDay, db_column='school_day_id', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.class_id.name} on {self.school_day_id.day.day}"
+    
+    class Meta:
+        verbose_name_plural = 'Class days'
 
 class ClassStudent(models.Model):
     class_id = models.ForeignKey(
