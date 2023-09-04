2c98148807c8bcafbdad095ab2d9a19079a20a8c
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-04 19:54:10 +0800

    feature: set up AssessmentDetail APIView and URL path

--- assessment/urls.py
+++ assessment/urls.py
@@ -8,4 +8,5 @@ from assessment.views import assessment_views
 # path('students/', student_views.StudentList.as_view(), name='student-list'),
 urlpatterns = [
 	path('assessments/', assessment_views.AssessmentList.as_view(), name='assessment-list'),
+	path('assessments/<str:assessment_pk>/', assessment_views.AssessmentDetail.as_view(), name='assessment-detail'),
 ]
--- assessment/views/assessment_views.py
+++ assessment/views/assessment_views.py
@@ -4,8 +4,6 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 
 from rest_framework.exceptions import NotFound
-from assessment.serializers import assessment_serializer
-from students.models.student_attendence_model import StudentAttendance
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from assessment.models.assessment_model import Assessment
@@ -35,43 +33,42 @@ class AssessmentList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-# class StudentAttendanceDetail(APIView):
+class AssessmentDetail(APIView):
 
-#     # Utility method to get an object or return a 404 response
-#     def get_object(self, student_attendance_pk):
-#         try:
-#             return StudentAttendance.objects.get(id=student_attendance_pk)
-#         except StudentAttendance.DoesNotExist:
-#             raise NotFound(detail="Object with this ID not found.")
+    # Utility method to get an object or return a 404 response
+    def get_object(self, assessment_pk):
+        try:
+            return Assessment.objects.get(id=assessment_pk)
+        except Assessment.DoesNotExist:
+            raise NotFound(detail="Object with this ID not found.")
 
-#     # Retrieve a specific entry by primary key
-#     def get(self, request, student_attendance_pk):
-#         student_attendance = self.get_object(student_attendance_pk)
-#         serializer = Serializer(student_attendance)
-#         return Response(serializer.data)
+    # Retrieve a specific entry by primary key
+    def get(self, request, assessment_pk):
+        assessment = self.get_object(assessment_pk)
+        serializer = AssessmentSerializer(assessment)
+        return Response(serializer.data)
 
-#     # Update a specific entry by primary key
-#     def put(self, request, student_attendance_pk):
-#         student_attendance = self.get_object(student_attendance_pk)
-#         serializer = Serializer(student_attendance, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # Update a specific entry by primary key
+    def put(self, request, assessment_pk):
+        assessment = self.get_object(assessment_pk)
+        serializer = AssessmentSerializer(assessment, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-#     # Partially update a specific entry by primary key
-#     def patch(self, request, student_attendance_pk):
-#         student_attendance = self.get_object(student_attendance_pk)
-#         serializer = Serializer(
-#             student_attendance, data=request.data, partial=True)
-#         if serializer.is_valid():
-#             updated_attendance = serializer.save()
-#             updated_serializer = StudentAttendanceDetailSerializer(updated_attendance, many=False)
-#             return Response(updated_serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # Partially update a specific entry by primary key
+    def patch(self, request, assessment_pk):
+        assessment = self.get_object(assessment_pk)
+        serializer = AssessmentSerializer(
+            assessment, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-#     # Delete a specific entry by primary key
-#     def delete(self, request, student_attendance_pk):
-#         student_attendance = self.get_object(student_attendance_pk)
-#         student_attendance.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
+    # Delete a specific entry by primary key
+    def delete(self, request, assessment_pk):
+        assessment = self.get_object(assessment_pk)
+        assessment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
