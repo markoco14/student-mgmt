df49b7d4942dcbba218d5b1020576cfd039dfc74
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-05 11:41:00 +0800

    feature: allow None due date and migrate, make create_assessment script

--- assessment/models/assessment_model.py
+++ assessment/models/assessment_model.py
@@ -17,7 +17,7 @@ class Assessment(models.Model):
     description = models.TextField()  # Students will be tested on...
     total_marks = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal('0.00'))
     date_announced = models.DateField()
-    date_due = models.DateField()
+    date_due = models.DateField(null=True, blank=True)
     assessment_type_id = models.ForeignKey(AssessmentType, db_column='assessment_type_id', related_name='assessments', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
