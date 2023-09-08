b1293a62ea9f42ef98c24ca09dae1766a52ca65c
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-08 20:33:00 +0800

    feature: add student field with student details to StudentEvaluation serializer

--- evaluation/serializers/student_evaluation_serializers.py
+++ evaluation/serializers/student_evaluation_serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from api.serializers.serializers import StudentSerializer
 
 from evaluation.models.student_evaluations import StudentEvaluation
 from evaluation.serializers.evaluation_attribute_serializers import EvaluationAttributeSerializer
@@ -7,6 +8,7 @@ from evaluation.serializers.evaluation_attribute_serializers import EvaluationAt
 
 class StudentEvaluationSerializer(serializers.ModelSerializer):
     evaluation_attribute = EvaluationAttributeSerializer(source="evaluation_attribute_id")
+    student = StudentSerializer(source="student_id")
     class Meta:
         model = StudentEvaluation
         fields = '__all__'
