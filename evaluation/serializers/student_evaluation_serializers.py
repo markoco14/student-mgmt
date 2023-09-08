d3836c5d12ab9eea3f5733a387387dabf4c14429
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-08 20:09:40 +0800

    feature: add evaluation data to StudentEvaluationSerializer

--- evaluation/serializers/student_evaluation_serializers.py
+++ evaluation/serializers/student_evaluation_serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 
 from evaluation.models.student_evaluations import StudentEvaluation
+from evaluation.serializers.evaluation_attribute_serializers import EvaluationAttributeSerializer
 
 
 
 class StudentEvaluationSerializer(serializers.ModelSerializer):
-    
+    evaluation_attribute = EvaluationAttributeSerializer(source="evaluation_attribute_id")
     class Meta:
         model = StudentEvaluation
         fields = '__all__'
