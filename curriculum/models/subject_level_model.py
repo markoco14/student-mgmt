21eb6d139bafe4582679c98eb0a47869187812f5
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-15 15:46:59 +0800

    feature: add 'subjects' reverse look up to level field on SubjectLevel model

--- curriculum/models/subject_level_model.py
+++ curriculum/models/subject_level_model.py
@@ -7,7 +7,7 @@ from curriculum.models.subject_model import Subject
 
 class SubjectLevel(models.Model):
     subject = models.ForeignKey(Subject, related_name="levels", on_delete=models.CASCADE)
-    level = models.ForeignKey(Level, on_delete=models.CASCADE)
+    level = models.ForeignKey(Level, related_name="subjects", on_delete=models.CASCADE)
     curriculum_description = models.TextField(null=True, blank=True)
 
     def __str__(self):
