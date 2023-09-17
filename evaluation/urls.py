f0bdef93a9e2635c5c69a9f4ffcd6a332219f3f1
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-17 13:05:19 +0800

    feat: set up TextEvaluationAttribute list/create methods and url path

--- evaluation/urls.py
+++ evaluation/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 
-from evaluation.views import evaluation_attribute_views, range_attribute_views
+from evaluation.views import evaluation_attribute_views, range_attribute_views, text_attribute_views
 from evaluation.views import student_evaluation_views
 
 
@@ -8,9 +8,14 @@ urlpatterns = [
 	# EVALUATION ATTRIBUTE ROUTES
     path('schools/<str:school_pk>/evaluation-attributes/',
          evaluation_attribute_views.EvaluationAttributeList.as_view(), name="evaluation-attribute-list"),
+	
+	# RANGE ATTRIBUTE
 	path('range-attributes/', range_attribute_views.RangeEvaluationAttributeList.as_view(), name="range-attribute-list"),
 	path('range-attributes/<str:range_attribute_pk>/', range_attribute_views.RangeEvaluationAttributeDetail.as_view(), name="range-attribute-detail"),
 
+	# TEXT ATTRIBUTE
+	path('text-attributes/', text_attribute_views.TextEvaluationAttributeList.as_view(), name="text-attribute-list"),
+	path('schools/<str:school_pk>/text-attributes/', text_attribute_views.TextEvaluationAttributeList.as_view(), name="text-attribute-list"),
 
     # DAILY REPORT PAGE ROUTES ROUTES
     path('schools/<str:school_pk>/daily-report-attributes/',
---
+++ evaluation/views/text_attribute_views.py
@@ -0,0 +1,73 @@
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.exceptions import NotFound
+from rest_framework.views import APIView
+
+from evaluation.models.evaluation_attribute_model import TextEvaluationAttribute
+from evaluation.serializers.evaluation_attribute_serializers import TextEvaluationAttributeSerializer
+
+
+class TextEvaluationAttributeList(APIView):
+    """
+    List all TextEvaluationAttributes, or create a new one.
+    """
+
+    def get(self, request, school_pk=None, format=None):
+        attributes = TextEvaluationAttribute.objects.all()
+
+        # Filter by school
+        if school_pk:
+            attributes = attributes.filter(school_id=school_pk)
+
+        serializer = TextEvaluationAttributeSerializer(attributes, many=True)
+
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = TextEvaluationAttributeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# class TextEvaluationAttributeDetail(APIView):
+#     """
+#     Retrieve, update or delete a TextEvaluationAttribute.
+#     """
+
+#     def get_object(self, range_attribute_pk):
+#         try:
+#             return TextEvaluationAttribute.objects.get(id=range_attribute_pk)
+#         except TextEvaluationAttribute.DoesNotExist:
+#             raise NotFound(detail="Object with this ID not found.")
+
+#     def get(self, request, range_attribute_pk, format=None):
+#         evaluation_attribute = self.get_object(range_attribute_pk)
+#         serializer = TextEvaluationAttributeSerializer(evaluation_attribute)
+#         return Response(serializer.data)
+
+#     def put(self, request, range_attribute_pk, format=None):
+#         evaluation_attribute = self.get_object(range_attribute_pk)
+#         serializer = TextEvaluationAttributeSerializer(
+#             evaluation_attribute, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#      # Partially update a specific entry by primary key
+#     def patch(self, request, range_attribute_pk):
+#         evaluation_attribute = self.get_object(range_attribute_pk)
+#         serializer = TextEvaluationAttributeSerializer(
+#             evaluation_attribute, data=request.data, partial=True)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     def delete(self, request, range_attribute_pk, format=None):
+#         evaluation_attribute = self.get_object(range_attribute_pk)
+#         evaluation_attribute.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
