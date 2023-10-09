851c8b2ed3170eddffc280a51243e01d08a9b28b
Mark O'Connor <mark.oconnor14@gmail.com>
2023-10-09 13:14:33 +0800

    refactor: remove schools/id/students route and move school_id to query params

--- students/urls.py
+++ students/urls.py
@@ -7,8 +7,6 @@ urlpatterns = [
 
     # STUDENT ROUTES
     path('students/', student_views.StudentList.as_view(), name='student-list'),
-    path('schools/<str:school_pk>/students/',
-         student_views.StudentList.as_view(), name='student-list'),
     path('students/<str:student_pk>/',
          student_views.StudentDetail.as_view(), name='student-detail'),
 
