66645d55b89866ee859eab05bdd2e143f1d3be00
Mark O'Connor <mark.oconnor14@gmail.com>
2024-04-07 12:57:27 +0800

    update settings for db

--- school_mgmt/settings.py
+++ school_mgmt/settings.py
@@ -173,13 +173,12 @@ else:
     # PSCALE DEVELOPMENT DB CREDENTIALS
     DATABASES = {
         'default': {
-            'ENGINE': 'django_psdb_engine',
+            'ENGINE': 'django.db.backends.mysql',
             'NAME': os.environ.get('DB_NAME'),
             'HOST': os.environ.get('DB_HOST'),
             'PORT': os.environ.get('DB_PORT'),
             'USER': os.environ.get('DB_USER'),
             'PASSWORD': os.environ.get('DB_PASSWORD'),
-            'OPTIONS': {'ssl': {'ca': os.environ.get('MYSQL_ATTR_SSL_CA')}}
         }
     }
 
