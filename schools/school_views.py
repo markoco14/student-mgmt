e51f3a5353e85c49458c478c88bc20a256c3605d
Mark <mark.oconnor14@gmail.com>
2025-02-16 14:17:05 +0800

    fix data error

--- schools/school_views.py
+++ schools/school_views.py
@@ -78,7 +78,7 @@ def add_school(request):
         return Response("School user serializer not valid")
     
     return Response(
-        data=school_serializer,
+        data=school_serializer.data,
         status=status.HTTP_201_CREATED
         )
 
