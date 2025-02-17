7a08959116d8bc6b6c8d75c456ece1f333a68714
Mark <mark.oconnor14@gmail.com>
2025-02-17 17:22:54 +0800

    rename school_views.py to views.py

--- schools/urls.py
+++ schools/urls.py
@@ -3,30 +3,27 @@ holds all school related url paths
 """
 
 from django.urls import path
-from api.views import user_views
 
-from schools import school_views
-
-
+from schools import views
 
 urlpatterns = [
     # SCHOOL ROUTES
-    path('schools/', school_views.list_schools, name="list-schools"),
+    path('schools/', views.list_schools, name="list-schools"),
     path('schools/<str:school_pk>/get/',
-         school_views.get_school_by_id, name="get-school-by-id"),
-    path('schools/add/', school_views.add_school, name="add-school"),
+         views.get_school_by_id, name="get-school-by-id"),
+    path('schools/add/', views.add_school, name="add-school"),
     path('schools/<str:school_pk>/update/',
-         school_views.update_school, name="update-school"),
+         views.update_school, name="update-school"),
     path('schools/<str:school_pk>/delete/',
-         school_views.delete_school, name="delete-school"),
+         views.delete_school, name="delete-school"),
 
     # SCHOOL DAY ROUTES
     path('school-days/',
-         school_views.SchoolDayList.as_view(), name='school-day-list'),
+         views.SchoolDayList.as_view(), name='school-day-list'),
     path('school-days/<str:school_day_pk>/',
-         school_views.SchoolDayDetail.as_view(), name='school-day-detail'),
+         views.SchoolDayDetail.as_view(), name='school-day-detail'),
 
     # SCHOOL-USER ROUTES
-    path('user-schools/', school_views.list_user_schools,
+    path('user-schools/', views.list_user_schools,
          name='get-schools-by-user-access'),
 ]
