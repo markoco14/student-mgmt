412e836f368a983fb248ec49541158cea7e59c11
Mark <mark.oconnor14@gmail.com>
2025-02-18 11:32:24 +0800

    fix user serializer because was setting is_active = 0

--- api/serializers/serializers.py
+++ api/serializers/serializers.py
@@ -9,6 +9,13 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            email=validated_data['email'], first_name=validated_data['first_name'], last_name=validated_data['last_name'], membership=validated_data['membership'])
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
     
 class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
