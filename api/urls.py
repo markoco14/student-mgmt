f13b50cfb06095a23faa0382c7317ab59aa51f84
Mark <mark.oconnor14@gmail.com>
2025-02-19 12:09:58 +0800

    move weekdays to schedule module

--- api/urls.py
+++ api/urls.py
@@ -7,15 +7,8 @@ from django.urls import path
 
 from api import views
 
-from schedule import views as schedule_views
-
 
 urlpatterns = [
     # GREETING ROUTE
     path('', views.healthCheck, name="health-check"),
-
- 
-    # SCHEDULE ROUTES
-    path('weekdays/', schedule_views.getWeekdays, name='weekday'),
-
 ]
--- school_mgmt/urls.py
+++ school_mgmt/urls.py
@@ -43,6 +43,7 @@ urlpatterns = [
 	path('', include('students.urls')),
 	path('', include('classes.urls')),
 	path('', include('schools.urls')),
+	path('', include('schedule.urls')),
 	path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
